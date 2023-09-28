d57a6d7e44d06b4946083b94c5442f760be1e523
Zclim <Zclim@users.noreply.github.com>
2023-09-28 17:45:45 +0000

    deploy: 0f79f8a0930cbf311ee64e540f53c00a85c5e1e7

---
+++ sui_analytics_indexer/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["FileFormat","FileType","Manifest"],"fn":["read_manifest","write_manifest"],"mod":["analytics_handler","analytics_metrics","csv_writer","errors","tables","writer"],"struct":["AnalyticsIndexerConfig","CheckpointUpdates","FileFormatIter","FileMetadata","FileTypeIter","ManifestV1"]};
